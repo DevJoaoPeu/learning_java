30ab239355d2fbe6453f5dceb13517b54a3fe007
DevJoaoPeu <joaocanaldroid@gmail.com>
2026-05-03 16:09:16 -0300

    refactor: use pattern from spring boot

---
+++ src/main/java/controller/ProductController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import app.model.Product;
+import org.springframework.web.bind.annotation.*;
+import service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController (ProductService service) {
+        productService = service;
+    }
+
+    @GetMapping
+    public List<Product> findAll () {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productService.create(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return productService.update(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+}
--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,6 +1,6 @@
 package service;
 
-import model.Product;
+import app.model.Product;
 import org.springframework.stereotype.Service;
 import repository.ProductRepository;
 
